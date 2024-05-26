1df19069f793d81133946e5400fd357c14fb95af
FilipeVBF <filipevbf@gmail.com>
2024-05-25 22:33:38 -0300

    Começo do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+import './styles.css'
 
 export function App() {
   return (
     <>
+      <Header />
       <Post 
         author='Diego Fernandes' 
         content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vero iure blanditiis necessitatibus, nemo cum maiores, ea similique nam at magnam! Quidem vel amet, deserunt placeat sequi molestiae vitae cum.'
