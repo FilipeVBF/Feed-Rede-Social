500a1a8d01be3f7c84e4a0805790ac848c6f989e
FilipeVBF <filipevbf@gmail.com>
2024-05-26 16:03:44 -0300

    Estilização do projeto 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Header } from "./components/Header";
-import './styles.css'
+import { Sidebar } from "./components/Sidebar";
+
+import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <>
       <Header />
-      <Post 
-        author='Diego Fernandes' 
-        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vero iure blanditiis necessitatibus, nemo cum maiores, ea similique nam at magnam! Quidem vel amet, deserunt placeat sequi molestiae vitae cum.'
-      />
-      <Post 
-        author='Gabriel Buzzi' 
-        content='Um novo post muito legal.'
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author='Diego Fernandes' 
+            content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vero iure blanditiis necessitatibus, nemo cum maiores, ea similique nam at magnam! Quidem vel amet, deserunt placeat sequi molestiae vitae cum.'
+          />
+          <Post 
+            author='Gabriel Buzzi' 
+            content='Um novo post muito legal.'
+          />
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+
+        <div className={styles.author}>
+          <img src="https://tse4.mm.bing.net/th/id/OIG3.W31wnCL197Wh0uhg5cYY?pid=ImgGn" />
+          <div className={styles.authorInfo}>
+            <strong>Diego Fernandes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title='26 de Maio às 13:20' dateTime="2024/05/26 13:20:30">Publicado há 1h</time>
+
+      </header>
+
+      <div className={styles.content}>
+
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href=''>jane.design/doctorcare</a></p>
+        <p><a href=''>#novoprojeto </a>
+        <a href=''>#nlw </a>
+        <a href=''>#rocketseat</a>
+        </p>
+
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+    </article>
+  )
+}
